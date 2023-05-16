c73d16c0cf946289c31ce70376de896a2d2f3157
StivenTorres17 <stiventorresbusiness@gmail.com>
2023-05-16 12:05:20 -0500

    feat: :construction: Creación de los directorios y ficheros necesarios para la construccion del API
    Se agregó el archivo main.js que contiene la instalación y configuración del servidor. Implementé API para contenido y usuarios, mientras dejaba los controladores, modelos y rutas en blanco para la comodidad de mis compañeros de equipo cuando trabajaban en esas áreas.  En package.json, se agregaron la descripción del proyecto, los autores y las dependencias para uso regular y de desarrollador. El backend utiliza tecnologías como Node.js, Express.js y MongoDB para una gestión de datos perfecta. Las dependencias de desarrollo incluyen herramientas como Nodemon para reinicios automáticos del servidor durante el desarrollo.  Al organizar el código base del back-end y configurar los archivos y directorios necesarios, este compromiso establece una base sólida para un mayor desarrollo. El proyecto tiene como objetivo proporcionar una plataforma de transmisión robusta con características que incluyen administración de usuarios, entrega de contenido y más.  Con este compromiso, la estructura de back-end está en su lugar, lo que permite una colaboración y un progreso fluidos para el proyecto CineBit. El trabajo futuro implicará la implementación de la lógica en los controladores, modelos y rutas para dar vida a la funcionalidad de back-end.
    
    Creados directorios y ficheros para el backend de CineBit.  Añadido código en el archivo main.js para la configuración del servidor.  Creada API para "content" (contenido) y "users" (usuarios).  Espacios en blanco dejados en los directorios de controllers, models y routes para mayor comodidad de los compañeros de equipo.  Descripción añadida en el package.json con información del proyecto.  Autores del proyecto especificados en el package.json.  Dependencias básicas agregadas en el package.json.  Dependencias para desarrolladores (devDependencies) agregadas en el package.json.

---
+++ mai.js
@@ -0,0 +1,31 @@
+// //? Se importa la librería express y los middlewares morgan y helmet.
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+
+// //? Se guarda en la constante app todos los métodos de la librería express.
+const app = express();
+
+// //? Se define el puerto en el que se ejecutará el servidor.
+const port = 3000;
+
+
+// //? Se importa las rutas para las APIs de usuarios y contenido.
+const users_routes = require("./API_Users/routes/users_routes");
+const content_routers = require("./API_Users/routes/content_routes");
+
+app.use(helmet());              //TODO: Ayuda a proteger la aplicación configurando diversos encabezados HTTP.
+app.use(morgan("combined"));    //TODO:  Imprime en consola información detallada sobre cada solicitud entrante al servidor.
+app.use(express.json());        //TODO: Convierte el cuerpo de la solicitud en un objeto JSON.
+
+
+// //? Se definen las rutas principales para el acceso a la información de la base de datos que contiene ambas colecciones.
+app.use('/users', users_routes);
+app.use('/Content', content_routers);
+
+// //? Se inicia el servidor y se muestra en consola el puerto en el que está corriendo.
+ app.listen(port, () => {
+     console.log("El servidor se ejecuta en el puerto http://localhost:" + port);
+   });
+
+//! http://localhost:3000/users
