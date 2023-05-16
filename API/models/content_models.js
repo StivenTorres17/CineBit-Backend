d0d1edfa7b18cd87e4d574a993c57354ba090d86
Andres <eamu.1992@gmail.com>
2023-05-16 12:26:39 -0500

    feat: :construction: Modelos de datos implementados para usuarios y contenido.
    El modelo de usuario incluye campos como nombre de usuario, correo electrónico, contraseña y rol. El modelo fue diseñado para almacenar información del usuario y admitir funciones de autenticación y autorización. @stivenTorres17 contribuyó al diseño y construcción del esquema de usuario, asegurando que se alinee con los requisitos del proyecto y las mejores prácticas.  El modelo de contenido representa películas, series documentales etc. Incluye campos como título, descripción, género, fecha de lanzamiento y duración. El esquema de contenido se elaboró ​​cuidadosamente para acomodar varios tipos de contenido y permitir una recuperación y gestión eficientes de los datos de contenido.  Al implementar estos modelos de datos, se ha establecido la base para almacenar y manipular datos de usuarios y contenidos. Los colaboradores ahora pueden aprovechar estos modelos para crear e interactuar con cuentas de usuario y transmitir contenido en la plataforma CineBit.  El trabajo adicional implicará la integración de estos modelos con los controladores, la implementación de la validación y persistencia de datos y la mejora de la funcionalidad general del backend.

---
+++ API/models/content_models.js
@@ -0,0 +1,12 @@
+const content_models = require("../models/content_models");
+
+//? De esta manera se llama todos los documentos de la colecion content
+exports.getAllcontent = (req, res) => {
+  
+  const { id } = req.params
+  content_models
+     
+  .findByIdAndUpdate(id, {username, name, lastNames, email, password, phone, isChidProfile, preferences}) //? Con el metodo find podemos enlistar nuestro documentos
+    .then((Content) => res.json(Content)) //? Con el metodo then nos muestra la informacion en un formato .json
+    .catch((err) => res.status(500).json({ error: err.message })); //? Con el metodo catch nos muestra el error
+};
---
+++ API/models/users_models.js
@@ -0,0 +1,56 @@
+const userModel = require("../models/users_models");
+
+exports.getAllUsers = (req, res) => {
+  userModel
+    .find( )
+    .then((users) => res.json(users))
+    .catch((err) => res.status(500).json({ error: err.message }));
+};
+
+//? createUser nos permite crear un usuario siempre y cuando cumpla con lo siguientes atributos
+exports.createUser = (req, res) => {
+  const {
+    username, name, lastNames, email, password, phone, isChidProfile, preferences} = req.body;
+  const newUser = new userModel({
+    username,
+    name,
+    lastNames,
+    email,
+    password,
+    phone,
+    isChidProfile,
+    preferences
+  });
+
+  newUser
+    .save()
+    .then(() => res.status(201).json({ success: "created" }))
+    .catch((err) => res.status(500).json({ error: err.message }));
+};
+
+//? updateUser nos permite actualizar la informacion del usuario por meido del id
+exports.updateUser = (req, res) => {
+  const { id } = req.params;
+  const {username, name, lastNames, email, password, phone, isChidProfile, preferences} = req.body;
+  userModel
+    .findByIdAndUpdate(id, {username, name, lastNames, email, password, phone, isChidProfile, preferences}, { new: true })
+    .then((user) => {
+      if (!user) throw new Error("user whith id ${id} not found");
+      res.status(200).json(user);
+    })
+
+    .catch((err) => res.status(500).json({ error: err.message }));
+};
+
+//? deleteUser nos permite eliminar un usuario por medio del id
+exports.deleteUser = (req, res) => {
+  const { id } = req.params;
+  userModel
+    .findByIdAndDelete(id)
+    .then((user) => {
+      if (!user) throw new Error("user whith id ${id} not found");
+      res.status(200).json({ message: "user deleted" });
+    })
+
+    .catch((err) => res.status(500).json({ error: err.message }));
+};
